382419eabba58f9d20f131229ef54a130aaefbbf
akhilrajeev3 <akhilnrajeev2001@gmail.com>
2024-09-16 12:14:50 +0530

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CustomerBlazorApp.Models;
+
+namespace CustomerBlazorApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Use the fully qualified name to avoid ambiguity if necessary
+        public DbSet<CustomerBlazorApp.Models.Customer> Customer { get; set; }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace CustomerBlazorApp.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SSN { get; set; }
+    }
+}
